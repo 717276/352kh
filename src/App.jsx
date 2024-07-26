0c8cf8e81d0c98c921bcd5ee66b65fde2c3ee6ad
gksqls9735 <gksqls9735@gmail.com>
2024-07-26 17:04:05 +0900

    feat: create tripdetail update mypage

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,57 @@
-import { Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Main from './pages/Main';
-import Trip from './pages/tour/Trip';
-import TripDetail from './pages/tour/TripDetail';
-import Build from './pages/tour/Build';
-import Review from './pages/review/Review';
-import ReviewWrite from './pages/review/ReviewWrite'
-import ReviewComment from './pages/review/ReviewComment';
-import Notice from './pages/notice/Notice';
-import Shop from './pages/shop/Shop';
-import Login from './pages/login/Login';
-import Register from './pages/login/Register';
-import ProductList from './pages/admin/ProductList';
-import ProductRegister from './pages/admin/ProductRegister';
-import ReviewModify from './pages/review/ReviewModify';
-import MyPage from './pages/mypage/MyPage';
-import Product from './pages/shop/Product';
-import ShopList from './pages/shop/ShopList';
-import QnA from './pages/csr/QnA';
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./pages/Main";
+import Trip from "./pages/tour/Trip";
+import TripDetail from "./pages/tour/TripDetail";
+import Build from "./pages/tour/Build";
+import Review from "./pages/review/Review";
+import ReviewWrite from "./pages/review/ReviewWrite";
+import ReviewComment from "./pages/review/ReviewComment";
+import Notice from "./pages/notice/Notice";
+import Shop from "./pages/shop/Shop";
+import Login from "./pages/login/Login";
+import Register from "./pages/login/Register";
+import ProductList from "./pages/admin/ProductList";
+import ProductRegister from "./pages/admin/ProductRegister";
+import ReviewModify from "./pages/review/ReviewModify";
+import MyPage from "./pages/mypage/MyPage";
+import Product from "./pages/shop/Product";
+import ShopList from "./pages/shop/ShopList";
+import QnA from "./pages/csr/QnA";
 
-function App() {  
+function App() {
   return (
-    <>    
-    <div className="Full">
-      <Header></Header>          
+    <>
+      <div className="Full">
+        <Header></Header>
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/trip" element={<Trip />} />
           <Route path="/tripDetail/:no" element={<TripDetail />} />
           <Route path="/build" element={<Build />} />
           <Route path="/review" element={<Review />} />
-          <Route path="/reviewWrite" element={<ReviewWrite/>}/> 
-          <Route path="/ReviewComment/:id" element={<ReviewComment/>}/>
+          <Route path="/reviewWrite" element={<ReviewWrite />} />
+          <Route path="/ReviewComment/:id" element={<ReviewComment />} />
           <Route path="/notice" element={<Notice />} />
-          <Route path="/shop" element={<Shop />} />      
-          <Route path="/login" element={<Login />} />      
-          <Route path="/register" element={<Register />} />   
-          <Route path="/admin/productList" element={<ProductList />} />    
-          <Route path="/admin/ProductRegister" element={<ProductRegister />} /> 
-          <Route path="/reviewComment/:id" element={<ReviewComment/>}/>
-          <Route path="/reviewModify/:id" element={<ReviewModify/>}/>
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/admin/productList" element={<ProductList />} />
+          <Route path="/admin/ProductRegister" element={<ProductRegister />} />
+          <Route path="/reviewComment/:id" element={<ReviewComment />} />
+          <Route path="/reviewModify/:id" element={<ReviewModify />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/shoplist/:category" element={<ShopList />} />
           <Route path="/mypage" element={<MyPage />} />
-          <Route path="/qna" element={<QnA/>}/>
-        </Routes>          
-      <Footer></Footer>
-    </div>
+          <Route path="/qna" element={<QnA />} />
+        </Routes>
+        <Footer></Footer>
+      </div>
     </>
-  )
+  );
 }
 
 export default App;
--- src/pages/tour/TripDetail.jsx
+++ src/pages/tour/TripDetail.jsx
@@ -1,4 +1,19 @@
-const TripDetail=()=>{
-    
-}
+import "../../components/css/tour/TripDetail.css";
+import place1 from "../../images/place/place1.png";
+const TripDetail = () => {
+  return (
+    <>
+      <div className="TripDetail">
+        <h1>투어 상세정보</h1>
+        <div className="placeInfo">
+          <img src={place1} />
+          <div className="placedesc">
+            <span>경복궁</span>
+            <span></span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 export default TripDetail;
